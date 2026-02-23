2054f2dd43f59369239516632ccce0b8a24deef3
Mark Hadjar <mark.hadjar@solarpointwp.com>
2026-02-23 13:17:29 -0500

    test(container): add service container unit tests
    Signed-off-by: Mark Hadjar <mark.hadjar@solarpointwp.com>

---
+++ packages/container/tests/Fixtures/ServiceImplementation.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the SolarPointWP framework.
+ *
+ * Copyright (c) 2026 Mark Hadjar <mark.hadjar@solarpointwp.com>
+ *
+ * For the full copyright and license information, please see the included LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace SolarPoint\Container\Tests\Fixtures;
+
+/**
+ * A concrete implementation of ServiceInterface, used as a fixture for interface binding tests.
+ *
+ * @internal
+ */
+final class ServiceImplementation implements ServiceInterface
+{
+}
---
+++ packages/container/tests/Fixtures/ServiceStub.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the SolarPointWP framework.
+ *
+ * Copyright (c) 2026 Mark Hadjar <mark.hadjar@solarpointwp.com>
+ *
+ * For the full copyright and license information, please see the included LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace SolarPoint\Container\Tests\Fixtures;
+
+/**
+ * A minimal instantiable class with no dependencies, used as a general-purpose test fixture.
+ *
+ * @internal
+ */
+final class ServiceStub
+{
+}
