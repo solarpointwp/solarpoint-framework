001cd022ac3a8f228af6e214f5928a26afe3567a
Mark Hadjar <mark.hadjar@solarpointwp.com>
2026-02-25 16:49:04 -0500

    feat(container): add abstract service provider
    Signed-off-by: Mark Hadjar <mark.hadjar@solarpointwp.com>

---
+++ packages/container/src/ServiceProvider/AbstractServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the SolarPointWP framework.
+ *
+ * Copyright (c) 2026 Mark Hadjar <mark.hadjar@solarpointwp.com>
+ *
+ * For the full copyright and license information, please see the included LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace SolarPoint\Container\ServiceProvider;
+
+use SolarPoint\Container\ContainerInterface;
+
+/**
+ * Base class for registering services into the container.
+ *
+ * @author Mark Hadjar <mark.hadjar@solarpointwp.com>
+ */
+abstract class AbstractServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        protected ContainerInterface $container,
+    ) {
+    }
+
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+
+    public function provides(): array
+    {
+        return [];
+    }
+}
